503acf81930a89aaf6a8d0be5a113cd1f9579066
kulotsystems <kulotsystems@gmail.com>
2022-12-11 17:54:53 +0800

    Lesson 10: Grid System.

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,4 +1,8 @@
 import { useEffect, useState } from 'react';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+
 
 const Notes = () => {
     const [notes, setNotes] = useState([]);
@@ -10,11 +14,32 @@ const Notes = () => {
     }, []);
 
     return (
-        <div>
-            {notes.map(note => (
-                <p key={note.id}>{note.title}</p>
-            ))}
-        </div>
+        <Container>
+            {/*<Grid container>*/}
+            {/*    <Grid item xs={12} sm={6} md={3}>*/}
+            {/*        <Paper>1</Paper>*/}
+            {/*    </Grid>*/}
+            {/*    <Grid item xs={12} sm={6} md={3}>*/}
+            {/*        <Paper>2</Paper>*/}
+            {/*    </Grid>*/}
+            {/*    <Grid item xs={12} sm={6} md={3}>*/}
+            {/*        <Paper>3</Paper>*/}
+            {/*    </Grid>*/}
+            {/*    <Grid item xs={12} sm={6} md={3}>*/}
+            {/*        <Paper>4</Paper>*/}
+            {/*    </Grid>*/}
+            {/*</Grid>*/}
+
+            <Grid container>
+                {notes.map(note => (
+                    <Grid item key={note.id} xs={12} md={6} lg={4}>
+                        <Paper>
+                            {note.title}
+                        </Paper>
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
     );
 };
 
