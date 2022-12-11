e76ed802f7681be04938cd656570b8aa816a3421
kulotsystems <kulotsystems@gmail.com>
2022-12-11 14:04:02 +0800

    Lesson 7: Text Fields.

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -3,13 +3,38 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import useClasses from '../hooks/useClasses.js';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
 
 const styles = (theme) => ({
-
+    field: {
+        marginTop: '20px !important',
+        marginBottom: '20px !important',
+        display: 'block !important'
+    }
 });
 
 const Create = () => {
     const classes = useClasses(styles);
+    const [title, setTitle] = useState('');
+    const [details, setDetails] = useState('');
+    const [titleError, setTitleError] = useState(false);
+    const [detailsError, setDetailsError] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setTitleError(false);
+        setDetailsError(false);
+
+        if(title === '')
+            setTitleError(true);
+        if(details === '')
+            setDetailsError(true);
+
+        if(title && details) {
+
+        }
+    };
 
     return (
         <Container>
@@ -22,15 +47,44 @@ const Create = () => {
                 Create a New Note
             </Typography>
 
-            <Button
-                type="submit"
-                color="secondary"
-                variant="contained"
-                onClick={() => console.log('you clicked me')}
-                endIcon={<KeyboardArrowRightIcon/>}
-            >
-                Submit
-            </Button>
+            <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+                <TextField
+                    label="Note Title"
+                    variant="outlined"
+                    color="secondary"
+                    fullWidth
+                    required
+                    className={classes.field.toString()}
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                    error={titleError}
+                />
+
+                <TextField
+                    label="Details"
+                    variant="outlined"
+                    color="secondary"
+                    fullWidth
+                    required
+                    className={classes.field.toString()}
+                    multiline
+                    rows={4}
+                    onChange={(e) => setDetails(e.target.value)}
+                    value={details}
+                    error={detailsError}
+                />
+
+                <Button
+                    type="submit"
+                    color="secondary"
+                    variant="contained"
+                    endIcon={<KeyboardArrowRightIcon/>}
+                >
+                    Submit
+                </Button>
+            </form>
+
+
         </Container>
     );
 };
