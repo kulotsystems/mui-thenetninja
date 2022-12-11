fcf9023348c75a82734aa33fc248dbe6ee9ce5b5
kulotsystems <kulotsystems@gmail.com>
2022-12-11 09:22:07 +0800

    Lesson 5: makeStyles Hook (Custom CSS).

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -2,8 +2,24 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import useClasses from '../hooks/useClasses.js';
+
+const styles = (theme) => ({
+    btn: {
+        fontSize: '60px !important',
+        backgroundColor: 'violet !important',
+        '&:hover': {
+            backgroundColor: 'blue !important',
+        }
+    },
+    title: {
+        textDecoration: 'underline !important',
+        marginBottom: '20px !important'
+    }
+});
 
 const Create = () => {
+    const classes = useClasses(styles);
 
     return (
         <Container>
@@ -12,6 +28,7 @@ const Create = () => {
                 component="h2"
                 gutterBottom
                 color="textSecondary"
+                className={classes.title.toString()}
             >
                 Create a New Note
             </Typography>
@@ -22,6 +39,7 @@ const Create = () => {
                 variant="contained"
                 onClick={() => console.log('you clicked me')}
                 endIcon={<KeyboardArrowRightIcon/>}
+                className={classes.btn.toString()}
             >
                 Submit
             </Button>
