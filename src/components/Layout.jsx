309065322e51a03c0927a7af9aa624e080151833
kulotsystems <kulotsystems@gmail.com>
2022-12-12 14:18:41 +0800

    Lesson 13: Permanent Drawer.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,22 @@
+import Drawer from '@mui/material/Drawer';
+import Typography from '@mui/material/Typography';
+
 import useClasses from '../hooks/useClasses.js';
 
+const drawerWidth = 240;
 const styles = (theme) => ({
     page: {
         background: '#f9f9f9 !important',
         width: '100% !important'
+    },
+    drawer: {
+        width: `${drawerWidth}px !important`
+    },
+    drawerPaper: {
+        width: `${drawerWidth}px !important`
+    },
+    root: {
+        display: 'flex !important'
     }
 });
 
@@ -11,10 +24,22 @@ const Layout = ({ children }) => {
     const classes = useClasses(styles);
 
     return (
-        <div>
+        <div className={classes.root}>
             {/*app bar  */}
 
             {/*app drawer*/}
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                anchor="left"
+                classes={{ paper: classes.drawerPaper }}
+            >
+                <div>
+                    <Typography variant="h5">
+                        Ninja Notes
+                    </Typography>
+                </div>
+            </Drawer>
 
             <div className={classes.page}>
                 { children }
