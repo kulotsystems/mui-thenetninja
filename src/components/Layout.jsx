78fba65afb00037a44feb6a93309b3cb3b8b2905
kulotsystems <kulotsystems@gmail.com>
2022-12-12 14:50:15 +0800

    Lesson 14: Lists & List Items.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,15 @@
 import Drawer from '@mui/material/Drawer';
 import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 
 import useClasses from '../hooks/useClasses.js';
+import SubjectOutlinedIcon from '@mui/icons-material/SubjectOutlined';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import { useNavigate, useLocation } from 'react-router-dom';
+import routes from '../routes.js';
 
 const drawerWidth = 240;
 const styles = (theme) => ({
@@ -17,11 +25,30 @@ const styles = (theme) => ({
     },
     root: {
         display: 'flex !important'
+    },
+    active: {
+        background: '#f4f4f4 !important',
+        color: '#d500f9 !important'
     }
 });
 
 const Layout = ({ children }) => {
-    const classes = useClasses(styles);
+    const classes  = useClasses(styles);
+    const navigate = useNavigate();
+    const currentLocation = useLocation().pathname;
+
+    const menuItems = [
+        {
+            text: 'My Notes',
+            icon: <SubjectOutlinedIcon color="secondary"/>,
+            path: routes.notes
+        },
+        {
+            text: 'Create Note',
+            icon: <AddCircleOutlineOutlinedIcon color="secondary"/>,
+            path: routes.create
+        }
+    ]
 
     return (
         <div className={classes.root}>
@@ -39,6 +66,20 @@ const Layout = ({ children }) => {
                         Ninja Notes
                     </Typography>
                 </div>
+
+                {/*list / links*/}
+                <List>
+                    {menuItems.map(item => (
+                        <ListItemButton
+                            key={item.text}
+                            onClick={() => navigate(item.path)}
+                            className={currentLocation === item.path ? classes.active : null}
+                        >
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.text}/>
+                        </ListItemButton>
+                    ))}
+                </List>
             </Drawer>
 
             <div className={classes.page}>
